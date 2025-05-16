08705a18cbcc7e0ebf38cdf79b17c4833387a1cf
amarnadh777 <amarnadh6565@gmail.com>
2025-05-16 13:17:13 +0530

    d

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,57 +2,68 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const otpGenerator = require("../utils/otpGenerator");
-const {sendEmail} = require("../utils/sendEmail")
-const{sendSms} = require("../utils/sendSms");
-const { PricingV2VoiceVoiceNumberInboundCallPrice } = require("twilio/lib/rest/pricing/v2/voice/number");
-  
+const { sendEmail } = require("../utils/sendEmail");
+const { sendSms } = require("../utils/sendSms");
+const {
+  PricingV2VoiceVoiceNumberInboundCallPrice,
+} = require("twilio/lib/rest/pricing/v2/voice/number");
+
 // Register user with validations, OTP generation and notifications
 exports.registerUser = async (req, res) => {
   try {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     const phoneRegex = /^\+91\d{10}$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 
     const { name, email, phone, password } = req.body;
 
     if (!name || !email || !phone || !password) {
-      return res.status(400).json({ message: "All fields are required",
-        messageType:"failure",
-           userId: null,
-       });
+      return res
+        .status(400)
+        .json({
+          message: "All fields are required",
+          messageType: "failure",
+          userId: "",
+        });
     }
 
     if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: "Invalid email format" ,
-        messageType:"failure",
-           userId: null,
-      });
+      return res
+        .status(400)
+        .json({
+          message: "Invalid email format",
+          messageType: "failure",
+          userId: "",
+        });
     }
 
     if (!phoneRegex.test(phone)) {
-      return res.status(400).json({ message: "Invalid phone number format. Use country code (+91)" 
+      return res.status(400).json({
+        message: "Invalid phone number format. Use country code (+91)",
 
-        ,
-        messageType:"failure",
-           userId: null,
+        messageType: "failure",
+        userId: "",
       });
     }
 
     if (!passwordRegex.test(password)) {
       return res.status(400).json({
-        message: "Password must be at least 8 characters long, include an uppercase letter, a number, and a special character.",
-        messageType:"failure",
-        userId:null
+        message:
+          "Password must be at least 8 characters long, include an uppercase letter, a number, and a special character.",
+        messageType: "failure",
+        userId: "",
       });
     }
 
     const existingUser = await User.findOne({ $or: [{ email }, { phone }] });
     if (existingUser) {
-      return res.status(400).json({ message: "Email or phone number already registered",
+      return res.status(400).json({
+        message: "Email or phone number already registered",
 
-        messageType:"failure",
-           userId: null,
-       });
+        messageType: "failure",
+        userId: "",
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -61,7 +72,7 @@ exports.registerUser = async (req, res) => {
     const emailOtp = otpGenerator(6);
     const otpExpiry = new Date(Date.now() + 10 * 60 * 1000);
 
-    await sendEmail(email, 'OTP Verification', `Your OTP is ${emailOtp}`);
+    await sendEmail(email, "OTP Verification", `Your OTP is ${emailOtp}`);
     // await sendSms(phone, `Hi, your OTP is ${phoneOtp}`);
 
     const newUser = new User({
@@ -80,15 +91,14 @@ exports.registerUser = async (req, res) => {
 
     res.status(201).json({
       message: "User registered successfully",
-      messageType:"success",
+      messageType: "success",
       userId: newUser._id,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error",
-      messageType:"failure",
-      userId:null
-     });
+    res
+      .status(500)
+      .json({ message: "Server error", messageType: "failure", userId: "" });
   }
 };
 // Verify OTPs for both email and phone
@@ -97,7 +107,9 @@ exports.verifyOtp = async (req, res) => {
     const { email, phone, emailOtp, phoneOtp } = req.body;
 
     if (!email || !phone || !emailOtp || !phoneOtp) {
-      return res.status(400).json({ message: "Email, phone number, and OTPs are required" });
+      return res
+        .status(400)
+        .json({ message: "Email, phone number, and OTPs are required" });
     }
 
     const user = await User.findOne({ $or: [{ email }, { phone }] });
@@ -132,43 +144,41 @@ exports.verifyOtp = async (req, res) => {
   }
 };
 
-
-
-
 exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(400).json({ message: "Email and password are required",
+      return res.status(400).json({
+        message: "Email and password are required",
 
-        messageType:"failure",
-           userId: null
-       });
+        messageType: "failure",
+        userId: "",
+      });
     }
 
     const userExist = await User.findOne({ email });
     if (!userExist) {
-      return res.status(400).json({ message: "User not found" ,
-        
-        messageType:"failure",
-           userId: null
+      return res.status(400).json({
+        message: "User not found",
+
+        messageType: "failure",
+        userId: "",
       });
     }
 
     const isMatch = await bcrypt.compare(password, userExist.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" ,
-          
-        messageType:"failure",
-           userId: null
+      return res.status(400).json({
+        message: "Invalid credentials",
+
+        messageType: "failure",
+        userId: "",
       });
     }
 
-    const token = jwt.sign(
-      { userId: userExist._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
+    const token = jwt.sign({ userId: userExist._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
     // Create a safe user object without password
     const user = {
@@ -180,132 +190,140 @@ exports.loginUser = async (req, res) => {
 
     res.json({
       message: "Logged in successfully",
-       messageType:"failure",
-           userId:  user._id,
-
+      messageType: "failure",
+      userId: user._id,
     });
-
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error",
-         messageType:"failure",
-     });
+    res.status(500).json({ message: "Server error", messageType: "failure" ,
+             messageType: "failure",
+              userId: "",
+    });
   }
 };
 
-
-
 exports.addAddress = async (req, res) => {
-    try {
-      const { street, city, state, zip, longitude, latitude, userId } = req.body;
-  
-      if (!street || !city || !state || !zip || !longitude || !latitude || !userId) {
-        return res.status(400).json({ message: "All fields are required" });
-      }
-  
-      const userExist = await User.findById(userId);
-      if (!userExist) {
-        return res.status(404).json({ message: "User not found" });
-      }
-  
-      userExist.address = {
-        street,
-        city,
-        state,
-        zip,
-        location: {
-          type: "Point",  // ✅ Important for GeoJSON!
-          coordinates: [parseFloat(longitude), parseFloat(latitude)]
-        }
-      };
-       
-      await userExist.save();
-  
-      res.json({ message: "Address updated successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server error" });
-    }
-  };
+  try {
+    const { street, city, state, zip, longitude, latitude, userId } = req.body;
 
-  exports.deleteAddressById = async (req, res) => {
-    try {
-      const { userId, addressId } = req.params;
-  
-      // Find the user by userId
-      const userExist = await User.findById(userId);
-      if (!userExist) {
-        return res.status(404).json({ message: "User not found" });
-      }
-  
-      // Check if the user has the specified addressId
-      const addressExists = userExist.address.id(addressId); // If it's a subdocument (array of addresses)
-      if (!addressExists) {
-        return res.status(404).json({ message: "Address not found" });
-      }
-  
-      // Remove the address
-      userExist.address.id(addressId).remove();
-  
-      // Save the updated user
-      await userExist.save();
-  
-      res.json({ message: "Address deleted successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server error" });
+    if (
+      !street ||
+      !city ||
+      !state ||
+      !zip ||
+      !longitude ||
+      !latitude ||
+      !userId
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-  };
-  
 
-  exports.updateAddressById = async (req, res) => {
-    try {
-      const { userId, addressId } = req.params; // Extract userId and addressId from request params
-      const { street, city, state, zip, longitude, latitude } = req.body; // Extract address fields from request body
-  
-      // Validate the required fields
-      if (!street || !city || !state || !zip || !longitude || !latitude) {
-        return res.status(400).json({ message: "All address fields are required" });
-      }
-  
-      // Find the user by userId
-      const userExist = await User.findById(userId);
-      if (!userExist) {
-        return res.status(404).json({ message: "User not found" });
-      }
-  
-      // Find the address by addressId within the user's address subdocument
-      const address = userExist.address.id(addressId);
-      if (!address) {
-        return res.status(404).json({ message: "Address not found" });
-      }
-  
-      // Update the address fields
-      address.street = street;
-      address.city = city;
-      address.state = state;
-      address.zip = zip;
-      address.location = {
-        type: "Point",  // Required for GeoJSON
-        coordinates: [parseFloat(longitude), parseFloat(latitude)]  // Ensure coordinates are floats
-      };
-  
-      // Save the updated user document
-      await userExist.save();
-  
-      res.json({ message: "Address updated successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server error" });
+    const userExist = await User.findById(userId);
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
-  // Resend new OTPs
+
+    userExist.address = {
+      street,
+      city,
+      state,
+      zip,
+      location: {
+        type: "Point", // ✅ Important for GeoJSON!
+        coordinates: [parseFloat(longitude), parseFloat(latitude)],
+      },
+    };
+
+    await userExist.save();
+
+    res.json({ message: "Address updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.deleteAddressById = async (req, res) => {
+  try {
+    const { userId, addressId } = req.params;
+
+    // Find the user by userId
+    const userExist = await User.findById(userId);
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check if the user has the specified addressId
+    const addressExists = userExist.address.id(addressId); // If it's a subdocument (array of addresses)
+    if (!addressExists) {
+      return res.status(404).json({ message: "Address not found" });
+    }
+
+    // Remove the address
+    userExist.address.id(addressId).remove();
+
+    // Save the updated user
+    await userExist.save();
+
+    res.json({ message: "Address deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.updateAddressById = async (req, res) => {
+  try {
+    const { userId, addressId } = req.params; // Extract userId and addressId from request params
+    const { street, city, state, zip, longitude, latitude } = req.body; // Extract address fields from request body
+
+    // Validate the required fields
+    if (!street || !city || !state || !zip || !longitude || !latitude) {
+      return res
+        .status(400)
+        .json({ message: "All address fields are required" });
+    }
+
+    // Find the user by userId
+    const userExist = await User.findById(userId);
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Find the address by addressId within the user's address subdocument
+    const address = userExist.address.id(addressId);
+    if (!address) {
+      return res.status(404).json({ message: "Address not found" });
+    }
+
+    // Update the address fields
+    address.street = street;
+    address.city = city;
+    address.state = state;
+    address.zip = zip;
+    address.location = {
+      type: "Point", // Required for GeoJSON
+      coordinates: [parseFloat(longitude), parseFloat(latitude)], // Ensure coordinates are floats
+    };
+
+    // Save the updated user document
+    await userExist.save();
+
+    res.json({ message: "Address updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+// Resend new OTPs
 exports.resendOtp = async (req, res) => {
   try {
     const { email, phone } = req.body;
 
     if (!email && !phone) {
-      return res.status(400).json({ message: "Email or phone number is required" });
+      return res
+        .status(400)
+        .json({ message: "Email or phone number is required" });
     }
 
     const user = await User.findOne({ $or: [{ email }, { phone }] });
@@ -317,7 +335,9 @@ exports.resendOtp = async (req, res) => {
     const timeDifference = (currentTime - user.verification.otpExpiry) / 60000;
 
     if (timeDifference < 5) {
-      return res.status(400).json({ message: "You can only request a new OTP after 5 minutes" });
+      return res
+        .status(400)
+        .json({ message: "You can only request a new OTP after 5 minutes" });
     }
 
     const emailOtp = otpGenerator(6);
@@ -330,7 +350,11 @@ exports.resendOtp = async (req, res) => {
 
     await user.save();
 
-    await sendEmail(user.email, 'OTP Verification', `Your new email OTP is ${emailOtp}`);
+    await sendEmail(
+      user.email,
+      "OTP Verification",
+      `Your new email OTP is ${emailOtp}`
+    );
     await sendSms(user.phone, `Your new phone OTP is ${phoneOtp}`);
 
     res.json({ message: "OTP sent successfully to email and phone" });
@@ -339,8 +363,6 @@ exports.resendOtp = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
-
-
 
 exports.forgotPassword = async (req, res) => {
   try {
