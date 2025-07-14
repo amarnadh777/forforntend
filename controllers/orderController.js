683538a73c335992a68f4152b179146a41e09cb8
amarnadh777 <amarnadh6565@gmail.com>
2025-07-14 21:56:37 +0530

    past orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -980,6 +980,7 @@ exports.getOrderPriceSummaryByaddressId = async (req, res) => {
 exports.getPastOrders = async (req, res) => {
   try {
     const userId = req.user._id;
+    console.log(userId)
 
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       return res.status(400).json({
@@ -989,7 +990,7 @@ exports.getPastOrders = async (req, res) => {
         data: null
       });
     }
-
+   
     // Fetch delivered and cancelled orders only
     const pastOrders = await Order.find({
       customerId: userId,
@@ -1003,13 +1004,15 @@ exports.getPastOrders = async (req, res) => {
       .lean();
 
     if (!pastOrders || pastOrders.length === 0) {
-      return res.status(404).json({
-        success: false,
-        messageType: "failure",
-        message: "No past orders found.",
-        data: null
-      });
+  return res.status(200).json({
+    success: true,
+    messageType: "success",
+    message: "No past orders found.",
+    data: {
+      orders: []
     }
+  });
+}
 
     // Format response
     const formattedOrders = pastOrders.map(order => {
